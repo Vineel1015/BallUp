214d74b9e3c810cadee01d650e852e643c1d6ac7
Vineel1015 <vineelnadakuditi@gmail.com>
2025-07-31 23:28:40 -0500

    updated version

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
@@ -23,6 +25,22 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator: React.FC = () => {
+  if (Platform.OS === 'web') {
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/games" element={<GameSearchScreen />} />
+        <Route path="/create-location" element={<CreateLocationScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/my-games" element={<MyGamesScreen />} />
+        <Route path="/create-game" element={<CreateGameScreen />} />
+      </Routes>
+    );
+  }
+
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen 
---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.web.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/BallUp/',
+  },
+  resolve: {
+    extensions: ['.web.js', '.js', '.web.ts', '.ts', '.web.tsx', '.tsx'],
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
